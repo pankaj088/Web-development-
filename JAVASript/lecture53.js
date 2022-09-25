5b778253be0bb39f8d7562d7947fa1a89acaa26e
pankaj kumar <pankaj0882000@gmail.com>
2022-09-25 19:19:40 +0530

    first commit JavaScript  25-09-2022

---
+++ JAVASript/lecture53.js
@@ -0,0 +1,22 @@
+console.log("This is lecture 53");
+
+// function use in javascript
+function greet(name) {
+    console.log(name + " is my college friend");
+}
+
+let name = "Pankaj";
+let name1 = "raushan";
+let name2 = "abhishek";
+let name3 = "nitish";
+//short form me hmlog greet() function use karte hai 
+greet(name);
+greet(name1);
+greet(name2);
+greet(name3);
+
+// long form mai yaise likhte hai
+// console.log(name + " is my college friend");
+// console.log(name1 + " is my college friend");
+// console.log(name2 + " is my college friend");
+// console.log(name3+ " is my college friend");
